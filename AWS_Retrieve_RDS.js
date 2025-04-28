ef1470f5b4daa03cd309cf16852b29d7fe70641d
ndmanlangit <ndmanlangit@gmail.com>
2025-04-28 11:33:12 +0800

    Add files via upload

---
+++ AWS_Retrieve_RDS.js
@@ -0,0 +1,41 @@
+﻿// Required libraries
+const mysql = require('mysql2');
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'us-west-2' }); //  AWS region
+const rds = new AWS.RDS();
+
+// Your RDS instance details
+const dbHost = 'rds-endpoint.amazonaws.com';  // Replace with your RDS endpoint
+const dbUser = 'db-username';
+const dbPassword = 'db-password';
+const dbName = 'database-name';
+
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+    host: dbHost,
+    user: dbUser,
+    password: dbPassword,
+    database: dbName,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Function to query the database
+const getData = async () => {
+    try {
+        pool.query('SELECT * FROM table_name', (err, results, fields) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                return;
+            }
+            console.log('Results:', results);
+        });
+    } catch (err) {
+        console.error('Error:', err);
+    }
+};
+
+// Execute the function to get data
+getData();
---
+++ AWS_Retrieve_from_S3.js
@@ -0,0 +1,23 @@
+﻿const AWS = require('aws-sdk');
+
+// Configure AWS
+AWS.config.update({
+    accessKeyId: 'ACCESS_KEY_ID',
+    secretAccessKey: 'SECRET_ACCESS_KEY',
+    region: 'REGION'
+});
+
+const s3 = new AWS.S3();
+
+const params = {
+    Bucket: 'bucket-name',
+    Key: 'file-key.txt' // e.g., folder/file.txt
+};
+
+s3.getObject(params, (err, data) => {
+    if (err) {
+        console.error('Error retrieving file:', err);
+    } else {
+        console.log('File content:', data.Body.toString()); // Buffer to string
+    }
+});
